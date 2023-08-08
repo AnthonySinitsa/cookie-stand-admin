d698f9ace129a602e12b031d8fcb32050e6fdd1c
AnthonySinitsa <itonyemail@gmail.com>
2023-08-08 00:29:27 +0000

    command

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,9 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="text-4xl">Cookie Stand Admin</h1>
-      <Link href="/overview">OverView</Link>
+      <h1 className="text-3xl" style={{ fontFamily: 'Arial' }}>Cookie Stand Admin</h1>
+      {/* <Link className="overviewLink" href="/overview">Overview</Link> */}
+      <a href="/overview"><button className="overviewButton" type="button">Overview</button></a>
     </header>
   );
 }
